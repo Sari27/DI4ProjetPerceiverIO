28fef7330117c9fabdfe21f2d0a9a6e96dd1d156
theo-boi <boisseau.theo@gmail.com>
2021-11-17 00:41:38 +0100

    Construction de la phrase prédite achevée.

--- random_masked_language.py
+++ random_masked_language.py
@@ -1,5 +1,5 @@
 import random
-from perceiver import bytes_tokenizer
+from perceiver import bytes_tokenizer as bt
 
 # Objectif : masquer un pourcentage de mots spécifique aléatoirement.
 
@@ -16,7 +16,6 @@ def chooseMaskedWords(str, maskPercentage):
             maskedwordsIndexesInData.append(word)
     #retourne la liste des mots masqués et leurs indices dans la liste des mots
     return maskedwords, maskedwordsIndexesInData
-
 
 
 def findIndexes(str, maskedwordsIndexesInData):
@@ -39,23 +38,52 @@ def findIndexes(str, maskedwordsIndexesInData):
     return maskedwordsIndexesInStr
 
 
-def stringWithMaskedWords(str, maskedwordsIndexesInStr):
+def inputWithMaskedWords(str, maskedwordsIndexesInStr):
     #définit la chaîne avec mots masqués
-    tokenizer = bytes_tokenizer.BytesTokenizer()
+    tokenizer = bt.BytesTokenizer()
     input_tokens = tokenizer.to_int(str)
-    even_number_of_masked_words_indexes_in_str = len(maskedwordsIndexesInStr) - len(maskedwordsIndexesInStr) % 2
-    for masked_word_index in range(0, even_number_of_masked_words_indexes_in_str, 2):
+    even_len_maskedwordsIndexesInStr = len(maskedwordsIndexesInStr) - len(maskedwordsIndexesInStr) % 2 #"corrige" l'ANOMALIE de analysis.ipynb
+    for masked_word_index in range(0, even_len_maskedwordsIndexesInStr, 2):
         input_tokens[maskedwordsIndexesInStr[masked_word_index]:maskedwordsIndexesInStr[masked_word_index+1]] = tokenizer.mask_token
     #retourne la chaîne en masquant les mots choisis
-    return tokenizer.to_string(input_tokens)
+    return input_tokens
+
+
+def stringWithMaskedWords(str, maskedwordsIndexesInStr):
+    return bt.BytesTokenizer().to_string(inputWithMaskedWords(str, maskedwordsIndexesInStr))
+
+
+def extractWordsByIndexes(str, words_indexes_in_str):
+    extracted_words = []
+    even_len_words_indexes_in_str = len(words_indexes_in_str) - len(words_indexes_in_str) % 2 #"corrige" l'ANOMALIE de analysis.ipynb
+    for index_even_iterator in range(0, even_len_words_indexes_in_str, 2):
+        extracted_words.append(
+            str[words_indexes_in_str[index_even_iterator]:words_indexes_in_str[index_even_iterator + 1]]
+        )
+    return extracted_words
+
+
+def stringWithNewWords(real_str, found_str, words_indexes_in_str):
+    #fournit la chaine avec les mots trouvés
+    #les Strings sont immuables, donc on utilise une version de found_str sous forme de liste pour la modifier
+    predicted_str = list(found_str[:len(real_str)])     #apres len(real_str), il n'y a que des 0
+    range_for_overwriting = [i for i in range(0,len(predicted_str))]    #les indices des caracteres a ecraser
+
+    even_len_words_indexes_in_str = len(words_indexes_in_str) - len(words_indexes_in_str) % 2 #"corrige" l'ANOMALIE de analysis.ipynb
+    for index_even_iterator in range(0, even_len_words_indexes_in_str, 2):
+        #on elimine tous les indexes des caracteres qui ne constituent pas les mots devines
+        for index_to_delete in range(words_indexes_in_str[index_even_iterator], words_indexes_in_str[index_even_iterator + 1]):
+            range_for_overwriting.remove(index_to_delete)
+
+    #on ecrase tous les caracteres qui ne constituent pas les mots devines par ceux de la String originale
+    for iterator_for_overwriting in range_for_overwriting:
+        predicted_str[iterator_for_overwriting] = real_str[iterator_for_overwriting]
+
+    #on reconstitue une String a partir de la liste de caracteres
+    predicted_str = "".join(predicted_str)
+    return predicted_str
 
 '''
-def stringWithNewWords(realStr, foundStr, maskedwordsIndexesInStr):
-    #fournit la chaine avec les mots trouvés
-    even_number_of_masked_words_indexes_in_str = len(maskedwordsIndexesInStr) - len(maskedwordsIndexesInStr) % 2
-    for masked_word_index in range(0, even_number_of_masked_words_indexes_in_str, 2):
-    
-    
 def succed(maskedWordsInRealStr, maskedWordsInFoundStr)
 
 '''
