e2eea414ba7d891dd53f7c21f2cdd3fd231e1641
Sari27 <48992399+Sari27@users.noreply.github.com>
2021-11-16 11:20:57 +0100

    Réglage import

--- perceiver/__init__.py
+++ perceiver/__init__.py
@@ -1,4 +1,4 @@
-import perceiver
-import position_encoding
-import io_processors
-import bytes_tokenizer
+from perceiver import perceiver
+from perceiver import position_encoding
+from perceiver import io_processors
+from perceiver import bytes_tokenizer
